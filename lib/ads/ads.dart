7b7d5949883fe501baf4e32f23846f178c57d25d
Carlos Peñaranda <67028929+carlostic26@users.noreply.github.com>
2023-09-19 11:32:25 -0500

    riverpod ok

--- lib/ads/ads.dart
+++
@@ -1,9 +0,0 @@
-class Ads {
-  // test:  ca-app-pub-3940256099942544/6300978111 || real: eobot ca-app-pub-4336409771912215/9135934709
-  String banner = 'ca-app-pub-4336409771912215/9135934709';
-
-  String intersticial = 'ca-app-pub-4336409771912215/6809154269';
-
-  // test: ca-app-pub-3940256099942544/3419835294 || real: robot ca-app-pub-4336409771912215/2462715149
-  String openAd = 'ca-app-pub-3940256099942544/3419835294';
-}
